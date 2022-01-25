6f08357c286bdb0dd0152a147029f14be5fe92f1
Ram Prakash Singh <ramprakash.singh@Pearson.com>
2022-01-25 22:11:35 +0530

    run exit

--- test/run.sh
+++ test/run.sh
@@ -1,36 +1,11 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
 
-./mvnw clean package
+./gradlew clean build
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
-cd ../initial
-
-./mvnw clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
 
 exit
